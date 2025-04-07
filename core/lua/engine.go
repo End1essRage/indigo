efef547f7f8fc927eafc06c0bbb71ccacacc2352
Nazar <end1essrage@mail.ru>
2025-04-07 23:52:54 +0300

    Добавил поддержку модулей для последущей реализации динамической передачи возможностей в скрипты

--- core/lua/engine.go
+++ core/lua/engine.go
@@ -22,10 +22,53 @@ type LuaCbData struct {
 	Data   string
 }
 
+type Module interface {
+	Apply(le *LuaEngine, L *lua.LState)
+}
+
+// LuaStateBuilder для конфигурации стейта под конкретный скрипт
+type LuaStateBuilder struct {
+	modules []Module
+	le      *LuaEngine
+}
+
+func NewStateBuilder(engine *LuaEngine) *LuaStateBuilder {
+	return &LuaStateBuilder{
+		le: engine,
+	}
+}
+
+func (b *LuaStateBuilder) WithModule(m Module) *LuaStateBuilder {
+	b.modules = append(b.modules, m)
+	return b
+}
+
+func (b *LuaStateBuilder) Build() *lua.LState {
+	L := lua.NewState()
+
+	// Базовые модули
+	base := CoreModule{}
+	base.Apply(b.le, L)
+
+	// Кастомные модули
+	for _, module := range b.modules {
+		module.Apply(b.le, L)
+	}
+
+	return L
+}
+
 // Lua engine wrapper
 type LuaEngine struct {
-	bot   Bot
-	cache Cache
+	bot     Bot
+	cache   Cache
+	modules []Module
+}
+
+func (le *LuaEngine) RegisterFunctions(L *lua.LState) {
+	for _, module := range le.modules {
+		module.Apply(le, L)
+	}
 }
 
 // интерфейс описывает какие ручки торчат в lua
@@ -40,83 +83,17 @@ type Cache interface {
 	SetString(key string, val string)
 }
 
+//работа с хранилищем
+
+//http клиент
+
 func NewLuaEngine(b Bot, c Cache) *LuaEngine {
 	return &LuaEngine{bot: b, cache: c}
 }
 
-// можно через паттерн билдер сделать возможность подключения луа/го модулей к своему боту, чтоб не тянуть кучу функционала
-// но это лучше скорее всего делать через cli или как-то чтоб и не раздувался бинарник, мб кодогенеративано подключать модули рядом
-func (le *LuaEngine) RegisterFunctions(L *lua.LState) {
-
-	//Логирование
-	L.SetGlobal("log", L.NewFunction(func(L *lua.LState) int {
-		msg := L.ToString(1)
-		logrus.Warnf("[LUA] %s", msg)
-		return 0
-	}))
-
-	// Кэш
-	L.SetGlobal("cache_get", L.NewFunction(func(L *lua.LState) int {
-		key := L.CheckString(1)
-		value := le.cache.GetString(key)
-		if value == "" {
-			L.Push(lua.LNil)
-		} else {
-			L.Push(lua.LString(value))
-		}
-		return 1
-	}))
-
-	L.SetGlobal("cache_set", L.NewFunction(func(L *lua.LState) int {
-		key := L.CheckString(1)
-		value := L.CheckString(2)
-		le.cache.SetString(key, value)
-		return 0
-	}))
-
-	//Отправка сообщения
-	L.SetGlobal("send_message", L.NewFunction(func(L *lua.LState) int {
-		logrus.Infof("send_message")
-
-		chatID := L.ToInt64(1)
-		text := L.ToString(2)
-
-		logrus.Infof("send_message ChatId: %v  text: %s", chatID, text)
-
-		if err := le.bot.SendMessage(chatID, text); err != nil {
-			logrus.Errorf("Error sending message: %v", err)
-		}
-		return 0
-	}))
-
-	//отправка клавиатуры
-	L.SetGlobal("send_keyboard", L.NewFunction(func(L *lua.LState) int {
-		chatID := L.CheckInt64(1)
-		text := L.CheckString(2)
-		meshTable := L.CheckTable(3) // Принимаем таблицу вместо JSON строки
-
-		// Конвертируем Lua таблицу в структуру Go
-		mesh := fromLuaTableToMeshInlineKeyboard(meshTable)
-
-		// Создаем и отправляем сообщение
-		msg := tgbotapi.NewMessage(chatID, text)
-		msg.ReplyMarkup = tgbotapi.InlineKeyboardMarkup{
-			InlineKeyboard: b.CreateInlineKeyboard(mesh),
-		}
-
-		if err := le.bot.Send(msg); err != nil {
-			logrus.Errorf("Error sending keyboard: %v", err)
-			L.Push(lua.LString("send failed"))
-			return 1
-		}
-
-		return 0
-	}))
-}
-
 func (le *LuaEngine) ExecuteScript(scriptPath string, lContext LuaContext) error {
 	logrus.Infof("ExecuteScript path:%s", scriptPath)
-	L := lua.NewState()
+	L := NewStateBuilder(le).WithModule(&CacheModule{}).WithModule(&BotModule{}).Build()
 	defer L.Close()
 
 	le.RegisterFunctions(L)
@@ -178,4 +155,80 @@ func fromLuaTableToMeshInlineKeyboard(lt *lua.LTable) b.MeshInlineKeyboard {
 	})
 
 	return mesh
+}
+
+// Реализации модулей
+type CoreModule struct{}
+type CacheModule struct{}
+type BotModule struct{}
+
+func (m *CoreModule) Apply(le *LuaEngine, L *lua.LState) {
+	//Логирование
+	L.SetGlobal("log", L.NewFunction(func(L *lua.LState) int {
+		msg := L.ToString(1)
+		logrus.Warnf("[LUA] %s", msg)
+		return 0
+	}))
+}
+
+func (m *CacheModule) Apply(le *LuaEngine, L *lua.LState) {
+	// Кэш
+	L.SetGlobal("cache_get", L.NewFunction(func(L *lua.LState) int {
+		key := L.CheckString(1)
+		value := le.cache.GetString(key)
+		if value == "" {
+			L.Push(lua.LNil)
+		} else {
+			L.Push(lua.LString(value))
+		}
+		return 0
+	}))
+
+	L.SetGlobal("cache_set", L.NewFunction(func(L *lua.LState) int {
+		key := L.CheckString(1)
+		value := L.CheckString(2)
+		le.cache.SetString(key, value)
+		return 0
+	}))
+}
+
+func (m *BotModule) Apply(le *LuaEngine, L *lua.LState) {
+	//Отправка сообщения
+	L.SetGlobal("send_message", L.NewFunction(func(L *lua.LState) int {
+		logrus.Infof("send_message")
+
+		chatID := L.ToInt64(1)
+		text := L.ToString(2)
+
+		logrus.Infof("send_message ChatId: %v  text: %s", chatID, text)
+
+		if err := le.bot.SendMessage(chatID, text); err != nil {
+			logrus.Errorf("Error sending message: %v", err)
+		}
+		return 0
+	}))
+
+	//отправка клавиатуры
+	L.SetGlobal("send_keyboard", L.NewFunction(func(L *lua.LState) int {
+		chatID := L.CheckInt64(1)
+		text := L.CheckString(2)
+		meshTable := L.CheckTable(3) // Принимаем таблицу вместо JSON строки
+
+		// Конвертируем Lua таблицу в структуру Go
+		mesh := fromLuaTableToMeshInlineKeyboard(meshTable)
+
+		// Создаем и отправляем сообщение
+		msg := tgbotapi.NewMessage(chatID, text)
+		msg.ReplyMarkup = tgbotapi.InlineKeyboardMarkup{
+			InlineKeyboard: b.CreateInlineKeyboard(mesh),
+		}
+
+		if err := le.bot.Send(msg); err != nil {
+			logrus.Errorf("Error sending keyboard: %v", err)
+			L.Push(lua.LString("send failed"))
+			return 1
+		}
+
+		return 0
+	}))
 }
